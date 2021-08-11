f1ef2e63f9cec3bdfd5c98f51ca67c5793a6971d
patryk <patryk.r.nowak@gmail.com>
2021-08-11 19:31:55 +0100

    set up of redux plus router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Routes from './routes';
+import { Provider } from 'react-redux';
+
+import ReduxStore from './store/index';
 
 const App = () => {
     return (
-      <>
-          Hello
-      </>
+      <Provider store={ReduxStore()}>
+        <Routes/>
+      </Provider>
     )
 }
 
---
+++ src/store/reducers/auth.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = {
+  isAuth: false,
+  user: null,
+  checkingAuth: false,
+};
+
+export default function (state = INITIAL_STATE, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
